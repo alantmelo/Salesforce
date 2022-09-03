db9bee95c55a912d05bacdd3ee5c5b24284d136e
Alan Tavares de Melo <alantmelo@gmail.com>
2022-09-03 11:31:50 -0300

    LWC Hero to Zero - 34. Methods And Two way Data Binding

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    name = "Alan Melo";
+    courseName = "Aura";
+    changeHandler(event) {
+        this.courseName = event.target.value;
+    }
+}
