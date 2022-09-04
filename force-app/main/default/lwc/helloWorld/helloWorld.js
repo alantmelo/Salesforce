58e2e972eb0812431c60379b00a0e7dd55513cdc
Alan Tavares de Melo <alantmelo@gmail.com>
2022-09-04 09:24:43 -0300

    LWC Zero to Hero - 35. @track properties

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,9 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
     name = "Alan Melo";
     courseName = "Aura";
     changeHandler(event) {
         this.courseName = event.target.value;
+    }
+    
+    /* @track (object and array)
+    @track address={
+        city: 'Maceió',
+        country: 'Brasil',
+    }
+    trackerHandler(event) {
+        this.address.city = event.target.value;
+    }
+    */
+    address={
+        city: 'Maceió',
+        country: 'Brasil',
+    }
+
+    trackerHandler(event) {
+        this.address = {...this.address, "city": event.target.value}
     }
 }
