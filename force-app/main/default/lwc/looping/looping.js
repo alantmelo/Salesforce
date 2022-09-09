3b857b7d8033628d987c8dadf3a38d17cbfe8de9
Alan Tavares de Melo <alantmelo@gmail.com>
2022-09-08 21:35:01 -0300

    Zero to Hero in LWC - 38. Template Looping

---
+++ force-app/main/default/lwc/looping/looping.js
@@ -0,0 +1,28 @@
+import { LightningElement } from 'lwc';
+
+export default class Looping extends LightningElement {
+    carlist = ["Argo", "Kwid", "C4", "Celta"];
+    carListHandler(event) {
+        console.log(event.target.value);
+        this.carlist = [...this.carlist, event.target.value];
+        event.target.value = "";
+    }
+
+    ceoList = [
+        {
+            id: 1,
+            company: "Google",
+            name: "Sundar Pichai"
+        },
+        {
+            id: 2,
+            company: "Apple",
+            name: "Tim Cook"
+        },
+        {
+            id: 3,
+            company: "Amazon",
+            name: "Jeff Bezoz"
+        },
+    ]
+}
